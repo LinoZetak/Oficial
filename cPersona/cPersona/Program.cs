cb5c9426501b210c5d8d27c61afc228e270c9f63
LinoZetak <linozeynthuaracalloarenas@gmail.com>
2024-08-20 10:18:20 -0500

    Add files via upload

---
+++ cPersona/cPersona/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BibliotecaTDA; 
+using cPersona;
+
+namespace cPersona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear personas
+            CPersona felipeV = new CPersona("Felipe V");
+            CPersona CarlosIII = new CPersona("Carlos III");
+            CPersona CarlosVI = new CPersona("Carlos VI");
+            CPersona MJoshepe = new CPersona("MJoshepe");
+
+            // Crear árbol genealógico
+            cArbolE arbol = cArbolE.Crear(felipeV);
+            cArbolE subArbolCarlosIII = arbol.Agregar(arbol, CarlosIII);
+            cArbolE subArbolCarlosVI = arbol.Agregar(arbol, CarlosVI);
+            cArbolE subArbolMJoshepe = arbol.Agregar(arbol, MJoshepe);
+
+            
+
+            // Recorridos del árbol
+            Console.WriteLine("Recorrido Preorden:");
+            arbol.PreOrden();
+
+            Console.WriteLine("\nRecorrido Posorden:");
+            arbol.PosOrden();
+
+            Console.WriteLine("\nRecorrido Inorden:");
+            arbol.InOrden();
+        }
+    }
+}
---
+++ cPersona/cPersona/cPersona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cPersona
+{
+    public class CPersona
+    {
+        private string aNombre;
+
+        public CPersona()
+        {
+            aNombre = " ";
+        }
+
+        public CPersona(string pNombre)
+        {
+            aNombre = pNombre;
+        }
+
+        public string Nombre
+        {  
+            get { return aNombre; }
+            set { aNombre = value; }
+        }
+
+        public override string ToString() //Sobrescribe el método ToString para devolver el nombre de la persona.
+                                          //Esto es útil cuando se imprimen los objetos de la clase en la consola, ya que se mostrará el nombre de la persona.
+        {
+            return Nombre;
+        }
+    }
+}
